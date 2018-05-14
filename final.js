479b2ea542c89d8fd498c64dfa2745054e84f3d8
Jan <janewaldable@gmail.com>
2018-05-14 16:02:56 +0200

    Added and formatted narrative text.

--- final.js
+++ final.js
@@ -565,7 +565,6 @@
         newDate.setMonth(month);
         newDate.setFullYear(year);
         if(formatDate(newDate) !== formatDate(currentDate)){
-            console.log("new");
             currentDate = newDate;
             loadNewReadings(currentDate, currentHour, sensorMappings);
         }

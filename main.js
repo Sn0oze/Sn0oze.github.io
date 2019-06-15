476eccfafed08e2a27790e62ba949e2d3d970094
Jan <janewaldable@gmail.com>
2019-06-15 14:39:08 +0200

    Updated marathon thumbnail, reworked colors, added enter animations

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 (function () {
-    /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+    // particlesJS.load(@dom-id, @path-json, @callback (optional));
     particlesJS.load('particles-js', 'assets/particlesjs-config.json', function() {
         console.log('callback - particles.js config loaded');
     });
 })();
+
 
 function select(event) {
     /*let isActive = event.target.classList.contains("active");
@@ -11,3 +12,13 @@ function select(event) {
         #event.target.addClass("active");
     }*/
 }
+
+document.addEventListener('scroll', e => {
+    if(document.scrollingElement.scrollTop !== 0){
+        const element = document.getElementById("header");
+        element.classList.add("stuck");
+    }else{
+        const element = document.getElementById("header");
+        element.classList.remove("stuck");
+    }
+});
